856d0432d98e75e81a0e5b2a3db559dc9d88b533
Colleen-Burke <colleen.a.burke@duke.edu>
2025-11-13 15:47:50 -0500

    Prepare data for PCA

--- 01_PCA.R
+++ 01_PCA.R
@@ -5,3 +5,21 @@ library(tidyverse)
 
 #Load Data
 Aim3_Data <- readRDS(here("Aim3_Data.rds"))
+
+
+# --- Inputs ---
+biomarkers <- c(
+  "IL_1beta","IL_6","TNF","IP_10","IFN_lambda1","IL_8",
+  "IL_12p70","IFN_alpha2","IFN_lambda2_3","GM_CSF","IFN_beta","IL_10",
+  "IFN_gamma","VILIP_1","MCP_1","sTREM_2","BDNF","TGF_beta1","VEGF",
+  "sTREM_1","beta_NGF","IL_18","sRAGE","CX3CL1"
+)
+
+# --- Prepare data ---
+base_df <- Aim3_Data |>
+  filter(biospecimen_id != "KPB030")
+
+pca_df <- base_df |>
+  select(biospecimen_id, all_of(biomarkers)) |>
+  drop_na()
+
