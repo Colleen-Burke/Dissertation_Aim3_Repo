d1162bf5b6e1da32458b8605fcc5475fba0c1914
Colleen-Burke <colleen.a.burke@duke.edu>
2025-11-13 16:03:34 -0500

    Social health factor (categorical) linear models with PC's.

--- 02_PCA_LM.R
+++ 02_PCA_LM.R
@@ -1,3 +1,48 @@
 # Load Data
 analysis_df <- readRDS(here("Aim3_Data_with_PCA.rds"))
 
+# ---------- Social Relationships Ordinal Models ----------
+lm_sr1 <- lm(PC1 ~ social_relationships_ordinal, data = analysis_df)
+summary(lm_sr1)
+
+lm_sr2 <- lm(PC2 ~ social_relationships_ordinal, data = analysis_df)
+summary(lm_sr2)
+
+
+# ---------- Social Relationships Nominal Models ----------
+lm_2sr1 <- lm(PC1 ~ social_relationships_nominal, data = analysis_df)
+summary(lm_2sr1)
+
+lm_2sr2 <- lm(PC2 ~ social_relationships_nominal, data = analysis_df)
+summary(lm_2sr2)
+
+# ---------- Social Activity Ordinal Models ----------
+lm_sa1 <- lm(PC1 ~ social_activities_ordinal, data = analysis_df)
+summary(lm_sa1)
+
+lm_sa2 <- lm(PC2 ~ social_activities_ordinal, data = analysis_df)
+summary(lm_sa2)
+
+
+# ---------- Social Activity Nominal Models ----------
+lm_2sa1 <- lm(PC1 ~ social_activities_nominal, data = analysis_df)
+summary(lm_2sa1)
+
+lm_2sa2 <- lm(PC2 ~ social_activities_nominal, data = analysis_df)
+summary(lm_2sa2)
+
+
+# ---------- Social Isolation Ordinal Models ----------
+lm_si1 <- lm(PC1 ~ isolation_cat_ordinal, data = analysis_df)
+summary(lm_si1)
+
+lm_si2 <- lm(PC2 ~ isolation_cat_ordinal, data = analysis_df)
+summary(lm_si2)
+
+
+# ---------- Social Isolation Nominal Models ----------
+lm_2si1 <- lm(PC1 ~ isolation_cat_nominal, data = analysis_df)
+summary(lm_2si1)
+
+lm_2si2 <- lm(PC2 ~ isolation_cat_nominal, data = analysis_df)
+summary(lm_2si2)
