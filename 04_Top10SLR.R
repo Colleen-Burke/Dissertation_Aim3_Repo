3e39b56da25c5f7f43be4e061018cc5ed8a942e8
Colleen-Burke <colleen.a.burke@duke.edu>
2025-11-14 14:04:33 -0500

    Set up exposures and outcomes

--- 04_Top10SLR.R
+++ 04_Top10SLR.R
@@ -6,3 +6,14 @@ library(dplyr)
 
 top10_analysis_df <- read_csv(here("analysis_df.csv"))
 
+# what you wanted
+exposures <- c("isolation_cat_ordinal", "isolation_cat_nominal", "isolation_cat_d")
+outcomes <- c(
+  "IL_10", "IFN_gamma", "IL_12p70", "IFN_alpha2", "CX3CL1",
+  "IFN_lambda1", "IFN_lambda2_3", "sTREM_1", "sRAGE", "GM_CSF"
+)
+
+# keep only the ones that actually exist in the data
+valid_exposures <- intersect(exposures, names(top10_analysis_df))
+valid_outcomes  <- intersect(outcomes,  names(top10_analysis_df))
+
