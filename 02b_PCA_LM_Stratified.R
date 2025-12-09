65e95c1da13da5f3d7e4c02d7370f55ace6d7f14
Colleen-Burke <colleen.a.burke@duke.edu>
2025-12-09 15:45:37 -0700

    Added social relationships PC1 and PC2 stratified models

--- 02b_PCA_LM_Stratified.R
+++ 02b_PCA_LM_Stratified.R
@@ -2,12 +2,36 @@ library(here)
 
 analysis_df <- readRDS(here("Aim3_Data_full.rds"))
 
-#--- Social Isolation and PC1 Stratified by Race ---------------------------------------
 # Split the data by race
 analysis_df_race1 <- analysis_df |> filter(race_dichotomized == 0)
 analysis_df_race2 <- analysis_df |> filter(race_dichotomized == 1)
 
-# Dichotomized models
+
+#--- Social Relationships (New Dichot.) and PC1 Stratified by Race ---------------------------------------
+lm_srd1_race1 <- lm(PC1 ~ social_relationships2_d, data = analysis_df_race1)
+lm_srd1_race2 <- lm(PC1 ~ social_relationships2_d, data = analysis_df_race2)
+
+summary(lm_srd1_race1)
+confint(lm_srd1_race1)
+
+summary(lm_srd1_race2)
+confint(lm_srd1_race2)
+
+
+
+#--- Social Relationships (New Dichot.) and PC2 Stratified by Race ---------------------------------------
+lm_srd2_race1 <- lm(PC2 ~ social_relationships2_d, data = analysis_df_race1)
+lm_srd2_race2 <- lm(PC2 ~ social_relationships2_d, data = analysis_df_race2)
+
+summary(lm_srd2_race1)
+confint(lm_srd2_race1)
+
+summary(lm_srd2_race2)
+confint(lm_srd2_race2)
+
+
+
+#--- Social Isolation and PC1 Stratified by Race ---------------------------------------
 lm_sid1_race1 <- lm(PC1 ~ isolation_cat_d, data = analysis_df_race1)
 lm_sid1_race2 <- lm(PC1 ~ isolation_cat_d, data = analysis_df_race2)
 
@@ -19,7 +43,6 @@ confint(lm_sid1_race2)
 
 
 #--- Social Isolation and PC2 Stratified by Race ---------------------------------------
-
 lm_sid2_race1 <- lm(PC2 ~ isolation_cat_d, data = analysis_df_race1)
 lm_sid2_race2 <- lm(PC2 ~ isolation_cat_d, data = analysis_df_race2)
 
@@ -33,24 +56,9 @@ confint(lm_sid2_race2)
 
 
 
-# New Dichotomized models
-lm_sid1_new_race1 <- lm(PC1 ~ isolation_cat2_d, data = analysis_df_race1)
-lm_sid1_new_race2 <- lm(PC1 ~ isolation_cat2_d, data = analysis_df_race2)
-
-summary(lm_sid1_new_race1)
-confint(lm_sid1_new_race1)
-
-summary(lm_sid1_new_race2)
-confint(lm_sid1_new_race2)
-
 
 
 #--- Social Composite with PC1 Stratified by race ---
-# Split the data by race
-analysis_df_race1 <- analysis_df |> filter(race_dichotomized == 0)
-analysis_df_race2 <- analysis_df |> filter(race_dichotomized == 1)
-
-# Dichotomized models
 lm_sf1_race1 <- lm(PC1 ~ social_factor, data = analysis_df_race1)
 lm_sf1_race2 <- lm(PC1 ~ social_factor, data = analysis_df_race2)
 
@@ -62,11 +70,6 @@ confint(lm_sf1_race2)
 
 
 #--- Social Composite with PC2 Stratified by race ---
-# Split the data by race
-analysis_df_race1 <- analysis_df |> filter(race_dichotomized == 0)
-analysis_df_race2 <- analysis_df |> filter(race_dichotomized == 1)
-
-# Dichotomized models
 lm_sf2_race1 <- lm(PC2 ~ social_factor, data = analysis_df_race1)
 lm_sf2_race2 <- lm(PC2 ~ social_factor, data = analysis_df_race2)
 
@@ -80,11 +83,6 @@ confint(lm_sf2_race2)
 
 
 #--- Social Composite Dichotomous with PC1 Stratified by race ---
-# Split the data by race
-analysis_df_race1 <- analysis_df |> filter(race_dichotomized == 0)
-analysis_df_race2 <- analysis_df |> filter(race_dichotomized == 1)
-
-# Dichotomized models
 lm_sfd1_race1 <- lm(PC1 ~ social_factor_d, data = analysis_df_race1)
 lm_sfd1_race2 <- lm(PC1 ~ social_factor_d, data = analysis_df_race2)
 
@@ -97,11 +95,6 @@ confint(lm_sfd1_race2)
 
 
 #--- Social Composite Dichotomous with PC2 Stratified by race ---
-# Split the data by race
-analysis_df_race1 <- analysis_df |> filter(race_dichotomized == 0)
-analysis_df_race2 <- analysis_df |> filter(race_dichotomized == 1)
-
-# Dichotomized models
 lm_sfd2_race1 <- lm(PC2 ~ social_factor_d, data = analysis_df_race1)
 lm_sfd2_race2 <- lm(PC2 ~ social_factor_d, data = analysis_df_race2)
 
