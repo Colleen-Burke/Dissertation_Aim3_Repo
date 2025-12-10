9f6ee12d71f58f22cc60138fcabe4e18edc4ef9d
Colleen-Burke <colleen.a.burke@duke.edu>
2025-12-10 07:35:16 -0700

    Renamed scripts for ordering

---
+++ 08_PC2_Top10SLR.R
@@ -0,0 +1,66 @@
+library(tidyverse)
+library(broom)
+library(here)
+library(dplyr)
+
+
+top10_analysis_df <- readRDS(here("Aim3_Data_full.rds"))
+#top10_analysis_df <- read_csv(here("Aim3_Data_full.rds"))
+
+# what you wanted
+exposures <- c("social_relationships_ordinal", "social_relationships_nominal", 
+               "social_relationships_d", "social_relationships2_d", 
+               "social_activities_ordinal", "social_activities_nominal", 
+               "social_activities_d", "social_activities2_d", "isolation_cat_ordinal", 
+               "isolation_cat_nominal", "isolation_cat_d", "isolation_cat2_d", 
+               "isolation_item_communicate", "isolation_item_visit", 
+               "isolation_item_communicate_d", "isolation_item_visit_d", "race_f", 
+               "race_dichotomized", "social_factor", "social_factor_d", 
+               "social_composite_sum")
+outcomes <- c(
+  
+)
+
+# keep only the ones that actually exist in the data
+valid_exposures <- intersect(exposures, names(top10_analysis_df))
+valid_outcomes  <- intersect(outcomes,  names(top10_analysis_df))
+
+# (optional) tell yourself what was missing
+setdiff(exposures, valid_exposures)
+setdiff(outcomes,  valid_outcomes)
+
+model_grid <- expand_grid(exposure = valid_exposures,
+                          outcome  = valid_outcomes) |>
+  mutate(
+    data = map2(exposure, outcome, ~
+                  top10_analysis_df |>
+                  select(all_of(c(.x, .y))) |>
+                  drop_na()
+    )
+  )
+
+view(model_grid)
+
+# Run each simple linear model
+model_results <- model_grid %>%
+  mutate(
+    model = map2(
+      exposure, outcome,
+      ~ lm(as.formula(paste(.y, "~", .x)), data = top10_analysis_df)
+    ),
+    tidy = map(model, broom::tidy, conf.int = TRUE, conf.level = 0.95)
+  ) %>%
+  select(exposure, outcome, tidy) %>%
+  unnest(tidy)
+
+# View summary table of results
+top10_SLM_summary <- model_results %>%
+  filter(term != "(Intercept)") %>%  # keep exposure term only
+  select(exposure, outcome, term, 
+         estimate, std.error, statistic, conf.low, conf.high, p.value)
+
+print(as.data.frame(top10_SLM_summary))
+
+write.csv(top10_SLM_summary, here("top10_SLM_summary.csv"))
+
+#saveRDS(top10_SLM_summary, here("top10_SLM_summary.rds"))
