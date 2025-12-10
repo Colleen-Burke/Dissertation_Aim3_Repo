c1de63f37a47192441b8a0e6bd8c00c4fa76faf8
Colleen-Burke <colleen.a.burke@duke.edu>
2025-12-10 07:39:41 -0700

    Created top 10 SLR for PC2

--- 08_PC2_Top10SLR.R
+++ 08_PC2_Top10SLR.R
@@ -4,7 +4,7 @@ library(here)
 library(dplyr)
 
 
-top10_analysis_df <- readRDS(here("Aim3_Data_full.rds"))
+PC2top10_analysis_df <- readRDS(here("Aim3_Data_full.rds"))
 #top10_analysis_df <- read_csv(here("Aim3_Data_full.rds"))
 
 # what you wanted
@@ -17,23 +17,24 @@ exposures <- c("social_relationships_ordinal", "social_relationships_nominal",
                "isolation_item_communicate_d", "isolation_item_visit_d", "race_f", 
                "race_dichotomized", "social_factor", "social_factor_d", 
                "social_composite_sum")
-outcomes <- c(
-  
+outcomes_PC2 <- c(
+  "TGF_beta1", "VEGF", "IL_18", "VILIP_1", "sRAGE", "sTREM_1", "CX3CL", "IL_6",
+  "IL_10", "IL_12p70"
 )
 
 # keep only the ones that actually exist in the data
-valid_exposures <- intersect(exposures, names(top10_analysis_df))
-valid_outcomes  <- intersect(outcomes,  names(top10_analysis_df))
+valid_exposures <- intersect(exposures, names(PC2top10_analysis_df))
+valid_PC2outcomes  <- intersect(outcomes_PC2,  names(PC2top10_analysis_df))
 
 # (optional) tell yourself what was missing
 setdiff(exposures, valid_exposures)
-setdiff(outcomes,  valid_outcomes)
+setdiff(outcomes_PC2,  valid_PC2outcomes)
 
 model_grid <- expand_grid(exposure = valid_exposures,
-                          outcome  = valid_outcomes) |>
+                          outcome_PC2  = valid_PC2outcomes) |>
   mutate(
-    data = map2(exposure, outcome, ~
-                  top10_analysis_df |>
+    data = map2(exposure, outcome_PC2, ~
+                  PC2top10_analysis_df |>
                   select(all_of(c(.x, .y))) |>
                   drop_na()
     )
@@ -45,22 +46,22 @@ view(model_grid)
 model_results <- model_grid %>%
   mutate(
     model = map2(
-      exposure, outcome,
-      ~ lm(as.formula(paste(.y, "~", .x)), data = top10_analysis_df)
+      exposure, outcome_PC2,
+      ~ lm(as.formula(paste(.y, "~", .x)), data = PC2top10_analysis_df)
     ),
     tidy = map(model, broom::tidy, conf.int = TRUE, conf.level = 0.95)
   ) %>%
-  select(exposure, outcome, tidy) %>%
+  select(exposure, outcome_PC2, tidy) %>%
   unnest(tidy)
 
 # View summary table of results
-top10_SLM_summary <- model_results %>%
+PC2top10_SLM_summary <- model_results %>%
   filter(term != "(Intercept)") %>%  # keep exposure term only
-  select(exposure, outcome, term, 
+  select(exposure, outcome_PC2, term, 
          estimate, std.error, statistic, conf.low, conf.high, p.value)
 
-print(as.data.frame(top10_SLM_summary))
+print(as.data.frame(PC2top10_SLM_summary))
 
-write.csv(top10_SLM_summary, here("top10_SLM_summary.csv"))
+write.csv(PC2top10_SLM_summary, here("PC2top10_SLM_summary.csv"))
 
-#saveRDS(top10_SLM_summary, here("top10_SLM_summary.rds"))
+#saveRDS(PC2top10_SLM_summary, here("PC2top10_SLM_summary.rds"))
