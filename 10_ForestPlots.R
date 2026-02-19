53211e2de08f9a32b7b24cfce1621c7b07112fc9
Colleen-Burke <colleen.a.burke@duke.edu>
2026-02-18 20:25:24 -0700

    Update all forest plot labels

--- 10_ForestPlots.R
+++ 10_ForestPlots.R
@@ -4,34 +4,14 @@ library(ggplot2)
 library(here)
 
 #------ PCA Models -------------
-ggplot(ggplot_data_pca,
-       aes(y = Exposure, x = Estimate)) +
-  
-  geom_vline(xintercept = 0, linetype = 2, color = "black") +
-  
-  geom_errorbarh(
-    aes(xmin = Lower_CI, xmax = Upper_CI),
-    height = 0.2,
-    color = "black"
-  ) +
-  
-  geom_point(
-    size = 2.5,
-    color = "black"
-  ) +
-  
-  facet_wrap(
-    ~ Outcome,
-    ncol = 1,
-    scales = "free_y",
-    strip.position = "top"
-  ) +
-  
-  labs(
-    x = "Estimate",
-    y = "Social Health Factors"
-  ) +
-  
+PCA_overall <- read.csv(here("PCA_overall.csv"))
+
+ggplot(PCA_overall, aes(y = Exposure, x = Estimate)) +
+  geom_vline(xintercept = 0, linetype = 2) +
+  geom_errorbarh(aes(xmin = Lower_CI, xmax = Upper_CI), height = 0.2) +
+  geom_point(size = 2.5) +
+  facet_wrap(~ Outcome, ncol = 1, scales = "free_y", strip.position = "top") +
+  labs(x = "Estimate", y = "Social Health Factors") +
   theme_minimal() +
   theme(
     strip.text.x = element_text(face = "bold", size = 11),
@@ -41,49 +21,51 @@ ggplot(ggplot_data_pca,
 
 
 
-#------ PC1 Model Stratified -------------
+#-------- PCA Stratified -------------
 PCA_overall_strat <- read.csv(here("PCA_overall_strat.csv"))
 
-PCA_overall_strat %>%
-  filter(Outcome != "PC2") %>%  # Drop PC2
-  mutate(Social = factor(Social, levels = c(1, 0)),
-         Race = factor(Race, 
-                       levels = c(0, 1, 2), 
-                       labels = c("Overall", "White", "Non-White")),
-         Race = fct_rev(Race)) %>%
-  ggplot(aes(y = Exposure, x = Estimate, color = Race)) +
-  facet_grid(rows = vars(Outcome, Social), scales = "free_y", space = "free_y",
-             labeller = labeller(Social = function(x) "")) +
-  geom_errorbarh(aes(xmin = Lower_CI, xmax = Upper_CI),
-                 height = 0.2,
-                 position = position_dodge(width = 0.6, preserve = "single")) +
-  geom_point(size = 2.5, position = position_dodge(width = 0.6, preserve = "single")) +
+ggplot_data <- PCA_overall_strat %>%
+  mutate(
+    Exposure = factor(Exposure,
+                      levels = c("Social_Roles", 
+                                 "Social_Activity", 
+                                 "Social_Isolation", 
+                                 "Social_Health"),
+                      labels = c("Social Roles", 
+                                 "Social Activity", 
+                                 "Social Isolation", 
+                                 "Social Health")),
+    Social = factor(Social, levels = c(1, 0)),
+    Race = factor(Race,
+                  levels = c(0, 1, 2),
+                  labels = c("Overall", "White", "Non-White")) %>%
+      fct_rev()
+  )
+
+ggplot(ggplot_data, aes(y = Exposure, x = Estimate, color = Race)) +
   geom_vline(xintercept = 0, linetype = 2) +
-  labs(x = "Estimate", y = "Social Health Factors", color = "Race") +
-  theme_minimal()
-
-
-
-#---------PC1 Top 10 -------------
-PC1_biomarkers <- read.csv(here("PC1_biomarkers.csv"))
-
-ggplot_data <- PC1_biomarkers |> 
-  filter(biomarker %in% c("IFN_lambda1", "IFN_lambda2_3", "sTREM_1"))
-
-ggplot(data = ggplot_data) +
-  facet_grid(rows = vars(exposure), scales = "free_y", space = "free_y") +
-  xlab("PC1 Top Contributing Biomarkers") + ylab("Estimates (95% CI)") +
-  geom_pointrange(aes(x = biomarker, y = estimate, ymin = conf.low, ymax = conf.high, color = exposure)) + 
-  geom_hline(yintercept = 0, lty = 2) +  
-  coord_flip() +  
-  theme_minimal() + theme(legend.position="none") 
-
+  geom_errorbarh(
+    aes(xmin = Lower_CI, xmax = Upper_CI),
+    height = 0.2,
+    position = position_dodge(width = 0.6)
+  ) +
+  geom_point(
+    size = 2.5,
+    position = position_dodge(width = 0.6)
+  ) +
+  facet_wrap(~ Outcome, ncol = 1, scales = "free_y", strip.position = "top") +
+  labs(x = "Estimate", y = "Social Factors", color = "Race") +
+  theme_minimal() +
+  theme(
+    strip.text.x = element_text(face = "bold", size = 11),
+    strip.background = element_blank(),
+    panel.spacing.y = unit(1.0, "lines")
+  )
 
 
 
 
 #---------PC1 Top 10 Stratified -------------
-
 PC1_biomarkers_strat <- read.csv(here("PC1_biomarkers_stratified.csv"))
 
 ggplot_data_strat <- PC1_biomarkers_strat |>
@@ -94,7 +76,21 @@ ggplot_data_strat <- PC1_biomarkers_strat |>
     Race = factor(race,
                   levels = c("Overall", "White", "Non-White"),
                   labels = c("Overall", "White", "Non-White")) |>
-      fct_rev()
+      fct_rev(),
+    biomarker = factor(biomarker,
+                       levels = c("IL_10", "IFN_gamma", "IL_12p70", "IFN_alpha2",
+                                  "CX3CL1", "IFN_lambda1", "IFN_lambda2_3",
+                                  "sTREM_1", "sRAGE", "GM_CSF"),
+                       labels = c("Interleukin-10 (IL-10)",
+                                  "Interferon Gamma (IFN-\u03b3)",
+                                  "Interleukin 12 (IL-12p70)",
+                                  "Interferon Alpha-2 (IFN-\u03b12)",
+                                  "Fractalkine (CX3CL1)",
+                                  "Interferon Lambda-1 (IFN-\u03bb1)",
+                                  "Interferon Lambda-2/3 (IFN-\u03bb2/3)",
+                                  "CineTreak Stream 1 (sTREM-1)",
+                                  "Soluble Receptor for Advanced Glycation End Products (sRAGE)",
+                                  "Granulocyte-Macrophage Colony-Stimulating Factor (GM-CSF)"))
   )
 
 ggplot(ggplot_data_strat, aes(y = Social, x = estimate, color = Race)) +
